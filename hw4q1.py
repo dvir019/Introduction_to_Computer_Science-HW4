6045e36cc43625954770afe71dc80e8a55c3d339
Dvir <dvir019@gmail.com>
2020-01-13 10:31:44 +0200

    Add the hw4q1.py template file

---
+++ hw4q1.py
@@ -0,0 +1,18 @@
+def main():
+    question = int(input())
+
+    if question == 1:
+        print('The answer to question 1 is a')
+    elif question == 2:
+        print('The answer to question 2 is a')
+    elif question == 3:
+        print('The answer to question 3 is a')
+    elif question == 4:
+        print('The answer to question 4 is a')
+    elif question == 5:
+        print('The answer to question 5 is a')
+    elif question == 6:
+        print('The answer to question 6 is a')
+    else:
+        print('Error')
+        exit()
