09a58cdd6730e03329d7316744a01517b2fa9c34
Dvir <dvir019@gmail.com>
2020-01-13 11:46:29 +0200

    Add the code for question 3

--- hw4q2.py
+++ hw4q2.py
@@ -1,5 +1,10 @@
 def digit_match(n, m):
-    pass
+    if n < 10:
+        return int(n == m % 10)
+    if m < 10:
+        return int(m == n % 10)
+
+    return int(n % 10 == m % 10) + digit_match(n // 10, m // 10)
 
 
 def main():
@@ -7,7 +12,8 @@ def main():
     n = int(input())
     print('Enter m:')
     m = int(input())
-    print('{} and {} share {} common digits!'.format(n, m, digit_match(n, m)))
+    print('{} and {} share {} common digits!'.format(n, m,
+                                                     digit_match(n, m)))
 
 
 if __name__ == '__main__':
